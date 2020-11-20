25d2822f7faa176fab29b7565b3040c2df9bd1bb
sue <ithwsw@users.noreply.github.com>
2020-11-20 14:20:11 +0800

    Update init.rb

--- init.rb
+++ init.rb
@@ -17,7 +17,8 @@ Redmine::Plugin.register :clipboard_image_paste do
   description 'Paste cropped image from clipboard as attachment'
   url         'http://www.redmine.org/plugins/clipboard_image_paste'
   version     '1.13'
-  requires_redmine :version_or_higher => '1.4.0'
+  #requires_redmine :version_or_higher => '1.4.0'
+  requires_redmine :version => "4.1.1"
 
   configfile = File.join(File.dirname(__FILE__), 'config', 'settings.yml')
   $clipboard_image_paste_config = YAML::load_file(configfile)
